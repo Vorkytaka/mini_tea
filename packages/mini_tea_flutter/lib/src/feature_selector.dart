4ff2bdb8244852bfa516288486a2d5be17451068
Vorkytaka <vorkytaka@gmail.com>
2024-12-27 01:46:09 +0300

    #41: Add dartdoc

--- packages/mini_tea_flutter/lib/src/feature_selector.dart
+++ packages/mini_tea_flutter/lib/src/feature_selector.dart
@@ -3,14 +3,46 @@ import 'package:mini_tea/feature.dart';
 
 import '../mini_tea_flutter.dart';
 
+/// Function representing a selector function to extract a value from the state.
 typedef FeatureWidgetSelector<S, T> = T Function(S state);
 
+/// A widget that selects a value from the state of a [Feature] and rebuilds.
+///
+/// [FeatureSelector] listens to the state stream of a [Feature] and rebuilds
+/// its child widget using a provided [builder] whenever the selected value changes.
+///
+/// ### Features:
+/// - Listens for state changes in the associated [Feature].
+/// - Optionally filters rebuilds using a [selector] function.
+/// - Supports injection of a specific [Feature] instance or resolves it from the widget tree.
+///
+/// ### Usage:
+///
+/// ```dart
+/// FeatureSelector<MyFeature, MyState, int>(
+///   selector: (state) => state.value,
+///   builder: (context, value) {
+///     return Text('Current value: $value');
+///   },
+/// );
+/// ```
 final class FeatureSelector<F extends Feature<S, dynamic, dynamic>, S, T>
     extends StatefulWidget {
+  /// A function to extract a value from the state.
   final FeatureWidgetSelector<S, T> selector;
+
+  /// A function that builds a widget based on the selected value.
   final FeatureWidgetBuilder<T> builder;
+
+  /// The [Feature] instance to be listened to.
+  /// If null, it is resolved from the widget tree.
   final F? feature;
 
+  /// Creates a [FeatureSelector] widget.
+  ///
+  /// - [selector]: A function to extract a value from the state.
+  /// - [builder]: A function that builds a widget based on the selected value.
+  /// - [feature]: The [Feature] instance to be listened to.
   const FeatureSelector({
     required this.selector,
     required this.builder,
